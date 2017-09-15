771d3fe8fd3a6de8e0dde45ea65159381acd093c
glenrs <grexsumsion@gmail.com>
2017-09-15 08:59:02 -0600

    Added tmp to .gitignore

---
+++ GSE2109_Colon_SCAN/parse.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+redirectedTempFolder=tmp
+Expression=$redirectedTempFolder/GSE2109_Colon_SCAN_Expression
+Clinical=$redirectedTempFolder/GSE2109_Colon_SCAN_Clinical
+outFile=data.tsv.gz
+
+#execute
+python parse.py $Clinical $Expression $outFile
+
+#clean-up
+rm -f .gitignore
+echo $outFile >> .gitignore
+echo $redirectedTempFolder >> .gitignore
+rm -r $redirectedTempFolder
