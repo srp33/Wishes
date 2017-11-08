50ba05cdf66beb7fb9e418e609d4c830f557e45e
Kimball Hill <hillkimball@gmail.com>
2017-11-08 16:05:06 -0700

    failing install.sh

--- BiomarkerBenchmark_GSE1456/install.sh
+++ BiomarkerBenchmark_GSE1456/install.sh
@@ -4,6 +4,7 @@ cp ../Helper/BiomarkerBenchmark/download.sh .
 cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
+cp badfile badlocation
 
 sed -e "s,{urlExpression},https://osf.io/4m3sv/download,g" -e "s,{urlClinical},https://osf.io/pxkfy/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
