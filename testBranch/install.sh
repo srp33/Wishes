072ea6155c6a26a8a436e043d2eda36250e7bd00
Kimball Hill <hillkimball@gmail.com>
2017-11-08 16:35:34 -0700

    fail download

--- testBranch/install.sh
+++ testBranch/install.sh
@@ -5,8 +5,8 @@ cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
 cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-echoe3 aw4fes43 awefcsfa
-wefse
 
-sed -e ",{urlExpression},https://osf.io/4m3svwnload,g" -e "s,{urlClinical},https://osf.io/pxkfy/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+
+
+sed -e "s,{urlExpression},https://osf.io/4m3svwnload,g" -e "s,{urlClinical},https://osf.io/pxkfy/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
