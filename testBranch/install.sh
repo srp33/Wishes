279e97343ff18a22dde102732503b6b92956d899
Kimball Hill <hillkimball@gmail.com>
2017-11-08 16:22:32 -0700

    messsage

--- testBranch/install.sh
+++ testBranch/install.sh
@@ -3,7 +3,10 @@
 cp ../Helper/BiomarkerBenchmark/download.sh .
 cp ../Helper/BiomarkerBenchmark/parse.py .
 cp ../Helper/BiomarkerBenchmark/parse.sh .
-cp ../Helper/BiomarkerBenchmark/cleanup.sh ./Badlocation
+cp ../Helper/BiomarkerBenchmark/cleanup.sh .
 
-sed -e "s,{urlExpression},https://osf.io/4m3svwnload,g" -e "s,{urlClinical},https://osf.io/pxkfy/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
+echoe3 aw4fes43 awefcsfa
+wefse
+
+sed -e ",{urlExpression},https://osf.io/4m3svwnload,g" -e "s,{urlClinical},https://osf.io/pxkfy/download,g" ../Helper/BiomarkerBenchmark/download.sh > download.sh 
 
