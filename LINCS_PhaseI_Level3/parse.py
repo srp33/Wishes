a480f95d21f83cd9c41f480ceaa3e8779ee550fd
glenrs <grexsumsion@gmail.com>
2018-01-02 12:01:20 -0700

    resubmit

--- LINCS_PhaseI_Level3/parse.py
+++ LINCS_PhaseI_Level3/parse.py
@@ -68,60 +68,46 @@ with gzip.open(pertMetrics, 'r') as f:
 
 instInfo =  gzip.open(instInfoFile, 'r')
 metaOut = gzip.open(metadataOut, 'w')
-instInfoSampleIds = []
-#for row in instInfo :
-#        rowList = row.strip('\n').split('\t')
-#        instInfoSampleIds.append(rowList[0])
-#instInfo.close()
-#intersectionValues = np.intersect1d(instInfoSampleIds,rowgrp["id"])
-#print("it worked")
+
 print("writing metadata file")
 try:
     headerList = instInfo.readline().strip('\n').split('\t')
     sigId = ""
     metaOut.write("Sample\tVariable\tValue\n")
+    indeci = 0
     for row in instInfo :
         rowList = row.strip('\n').split('\t')
-	if(np.any(rowgrp["id"][:,] ==  rowList[0])) :
-            print("matched")
-#            didItWork = np.where(rowgrp["id"]==rowList[0])
-            instInfoSampleIds.append(rowList[0])
-            print(rowList[0])        
-            for i in range(len(rowList) - 1 ):
-                if(str(rowList[i + 1]) != "-666") and (str(rowList[i + 1]) != "-666.0") :
-                    metaOut.write(str(rowList[0]) + '\t' + str(headerList[i + 1]) + '\t' + str(rowList[i + 1]) + '\n')
+        indeci = indeci + 1
+        print(str(indeci) + " of 1319138")
+        for i in range(len(rowList) - 1 ):
+             if(str(rowList[i + 1]) != "-666") and (str(rowList[i + 1]) != "-666.0") :
+                 metaOut.write(str(rowList[0]) + '\t' + str(headerList[i + 1]) + '\t' + str(rowList[i + 1]) + '\n')
                 
-                if (headerList[i + 1] == "cell_id") :
-                    try :
-                        cellIdList = cellInfoDict[rowList[i + 1]] ##This will add the cellInfo to the metadata.tsv.gz
-                        for j in range(len(cellIdList)) :
-                            if(str(cellIdList[j]) != "-666") :
-                                metaOut.write(str(rowList[0]) + '\t' + str(cellHeaderList[j]) + '\t' + str(cellIdList[j]) + '\n')
-                    except :
-                        continue ## This catches SNUC4 that doesn't have any cellInfo, but is included in the sigInfo file
-                elif (headerList[i + 1]  == "pert_id") :
-                    try :
-                        pertInfoIdList = pertInfoDict[rowList[i + 1]] ##This will add the pertInfo metadata to the metadata.tsv.gz
-                        for j in range(len(pertInfoIdList)) :
-                            if(str(pertInfoIdList[j]) != "-666") :
-                                metaOut.write(str(rowList[0]) + '\t' + str(pertInfoHeaderList[j]) + '\t' + str(pertInfoIdList[j]) + '\n')
-                        pertMetricsIdList = pertMetricsDict[rowList[i + 1]] ##This will add the pertMetrics metadata to the metadata.tsv.gz 
-                        for j in range(len(pertMetricsIdList)) :
-                            if(str(pertMetricsIdList[j]) != "-666") :
-                                metaOut.write(str(rowList[0]) + '\t' + str(pertMetricsHeaderList[j]) + '\t' + str(pertMetricsIdList[j]) + '\n')
-                    except :
-                        continue
-        else :
-#        except :
-            print("metadata lines didn't match")
-            continue
+             if (headerList[i + 1] == "cell_id") :
+                 try :
+                     cellIdList = cellInfoDict[rowList[i + 1]] ##This will add the cellInfo to the metadata.tsv.gz
+                     for j in range(len(cellIdList)) :
+                         if(str(cellIdList[j]) != "-666") :
+                             metaOut.write(str(rowList[0]) + '\t' + str(cellHeaderList[j]) + '\t' + str(cellIdList[j]) + '\n')
+                 except :
+                     continue ## This catches SNUC4 that doesn't have any cellInfo, but is included in the sigInfo file
+             elif (headerList[i + 1]  == "pert_id") :
+                 try :
+                     pertInfoIdList = pertInfoDict[rowList[i + 1]] ##This will add the pertInfo metadata to the metadata.tsv.gz
+                     for j in range(len(pertInfoIdList)) :
+                         if(str(pertInfoIdList[j]) != "-666") :
+                             metaOut.write(str(rowList[0]) + '\t' + str(pertInfoHeaderList[j]) + '\t' + str(pertInfoIdList[j]) + '\n')
+                     pertMetricsIdList = pertMetricsDict[rowList[i + 1]] ##This will add the pertMetrics metadata to the metadata.tsv.gz 
+                     for j in range(len(pertMetricsIdList)) :
+                         if(str(pertMetricsIdList[j]) != "-666") :
+                             metaOut.write(str(rowList[0]) + '\t' + str(pertMetricsHeaderList[j]) + '\t' + str(pertMetricsIdList[j]) + '\n')
+                 except :
+                     continue
 finally:
     instInfo.close()
     metaOut.close()
 
 print("writing expression file")
-
-instInfoSampleIds = np.asarray(instInfoSampleIds)
 
 f = gzip.open(dataOut, 'w')
 try :
@@ -133,16 +119,8 @@ try :
     index = 0
     for line in subsubgrpData["matrix"] :
         a = np.asarray(line).astype(str)
-#            didItWork = np.where(intersectionValues==rowgrp["id"][index])
-	if(np.any(instInfoSampleIds[:,0] == rowgrp["id"][index])) :
-#        if(np.any(instInfoSampleIds[:,0] == rowgrp["id"][index])) :
-#            didItWork = np.where(instInfoSampleIds==rowgrp["id"][index])
-            f.write(rowgrp["id"][index] + '\t' + '\t'.join(a) + '\n')
-            index = index + 1
-            print(str(index) + " of ")
-        else :
-            index = index + 1
-            print("didn't match " + rowgrp["id"][index])
-            continue
+        f.write(rowgrp["id"][index] + '\t' + '\t'.join(a) + '\n')
+        index = index + 1
+        print(str(index) + " of 1319138")
 finally :
     f.close()
