67317bcac2721603b672efae2c68ea77a09b8ae1
glenrs <grexsumsion@gmail.com>
2018-02-07 10:46:43 -0700

    fixed test files, added sig metrics file

--- LINCS_PhaseI_Level5/parse.sh
+++ LINCS_PhaseI_Level5/parse.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#SBATCH --time=15:00:00   # walltime
+#SBATCH --time=4:00:00   # walltime
 #SBATCH --ntasks=8   # number of processor cores (i.e. tasks)
 #SBATCH --nodes=1   # number of nodes
 #SBATCH --mem-per-cpu=32768M   # memory per CPU core
@@ -11,8 +11,12 @@ softwareFolder=Software
 
 #Downloaded in download.sh
 sigFileName=$redirectedTempFolder/GSE92742_Broad_LINCS_sig_info.txt.gz
+sig_metrics=$redirectedTempFolder/GSE92742_Broad_LINCS_sig_metrics.txt.gz
 gctxFileName=$redirectedTempFolder/LINCS_PhaseI_Level5.gctx
 geneFile=$redirectedTempFolder/GSE92742_Broad_LINCS_gene_info.txt.gz
+cellInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_cell_info.txt.gz
+pertInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_info.txt.gz
+pertMetrics=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_metrics.txt.gz
 
 #Downloaded and Installed in install.sh
 minicondaPath=$softwareFolder/miniconda/bin/
@@ -21,18 +25,14 @@ minicondaPath=$softwareFolder/miniconda/bin/
 dataOutFile=data.tsv.gz
 metadataOutFile=metadata.tsv.gz
 
-#Other Metadata files that are included in final output
-cellInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_cell_info.txt.gz
-pertInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_info.txt.gz
-pertMetrics=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_metrics.txt.gz
-
 rm -f $metadataOutFile
 rm -f $dataOutFile
 
 #miniconda is used to store panda software in environments need to activate environment
 echo "Setting up environment"
-cd $minicondaPath
-source activate lincs_env
-cd ../../..
+#cd $minicondaPath
+#source activate lincs_env
+#cd ../../..
+source activate WishBuilderDependencies
 
-python parse.py $sigFileName $gctxFileName $metadataOutFile $dataOutFile $geneFile $cellInfo $pertInfo $pertMetrics
+python parse.py $sigFileName $gctxFileName $metadataOutFile $dataOutFile $geneFile $cellInfo $pertInfo $pertMetrics $sig_metrics
