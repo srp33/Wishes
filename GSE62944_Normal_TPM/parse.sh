c83b736eb54c835d8e2511de03acac9cd1dd1b97
glenrs <grexsumsion@gmail.com>
2018-02-23 14:14:58 -0700

    converted abbreviations and added description info

--- GSE62944_Normal_TPM/parse.sh
+++ GSE62944_Normal_TPM/parse.sh
@@ -1,18 +1,14 @@
 #! /bin/bash
 
 redirectedTempFolder=tmp
-PatientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_Normal_CancerType_Samples.txt
-NormalTPM=$redirectedTempFolder/GSM1697009_06_01_15_TCGA_24.normal_Rsubread_TPM.txt
-transposedNormalTPM=$redirectedTempFolder/transposedNormalTPM.txt
-dataOutFile=data.tsv
-metadataOutFile=metadata.tsv
+PatientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_Normal_CancerType_Samples.txt.gz
+NormalTPM=$redirectedTempFolder/GSM1697009_06_01_15_TCGA_24.normal_Rsubread_TPM.txt.gz
+tcgaHtml=$redirectedTempFolder/"tcga_abbreviations.html"
+nameToAbbreviation=$redirectedTempFolder/"nameToAbbreviation.txt"
 dataOutFilegz=data.tsv.gz
 metadataOutFilegz=metadata.tsv.gz
 
-rm -f $metadataOutFilegz
-rm -f $dataOutFilegz
+source activate WishBuilderDependencies
 
-python parse.py $PatientCancerType $NormalTPM $transposedNormalTPM $dataOutFile $metadataOutFile
-
-gzip $metadataOutFile
-gzip $dataOutFile
+Rscript scrapeWebTCGA.R $tcgaHtml $nameToAbbreviation
+python parse.py $PatientCancerType $NormalTPM $dataOutFilegz $metadataOutFilegz $nameToAbbreviation
