72aa77b2ccb0292272f4b30e6971671227bfd42c
Benjamin Cookson <btc36@m8int01.fsl.byu.edu>
2018-03-09 16:00:05 -0700

    Added extra Metadata

--- CCLE_mRNA_gene_kallisto_Tatlow/download.sh
+++ CCLE_mRNA_gene_kallisto_Tatlow/download.sh
@@ -15,3 +15,27 @@ fileName=$redirectedTempFolder/CCLE_tpm_Clinical.tsv
 
 wget -O $fileName $url
 
+#downloading Pharmacological profiling responses
+url="https://portals.broadinstitute.org/ccle_legacy/downloadFile/DefaultSystemRoot/exp_10/ds_27/CCLE_NP24.2009_Drug_data_2015.02.24.csv?downloadff=true&fileId=20777"
+fileName=$redirectedTempFolder/CCLE_NP24.2009_Drug_data_2015.02.24.csv
+
+wget -O $fileName $url
+
+#downloading Drug Information
+url="https://portals.broadinstitute.org/ccle_legacy/downloadFile/DefaultSystemRoot/exp_10/ds_27/CCLE_NP24.2009_profiling_2012.02.20.csv?downloadff=true&fileId=3422"
+fileName=$redirectedTempFolder/CCLE_NP24.2009_profiling_2012.02.20.csv
+
+wget -O $fileName $url
+
+#downloading extra Metadata 1
+url="https://portals.broadinstitute.org/ccle_legacy/downloadFile/DefaultSystemRoot/exp_10/ds_20/CCLE_copynumber_byGene_2013-12-03.txt.gz?downloadff=true&fileId=17599"
+fileName=$redirectedTempFolder/CCLE_copynumber_byGene_2013-12-03.txt.gz
+
+wget -O $fileName $url
+gunzip $fileName
+
+#downloading extra Metadata 2
+url="https://portals.broadinstitute.org/ccle_legacy/downloadFile/DefaultSystemRoot/exp_10/ds_32/ccle2maf_20170805f.txt?downloadff=true&fileId=27366"
+fileName=$redirectedTempFolder/ccle2maf_20170805f.txt
+
+wget -O $fileName $url
--- CCLE_mRNA_gene_kallisto_Tatlow/parse.sh
+++ CCLE_mRNA_gene_kallisto_Tatlow/parse.sh
@@ -3,6 +3,10 @@
 redirectedTempFolder=tmp
 expressionData=$redirectedTempFolder/CCLE_tpm_Expression.tsv
 clinicalAnnotations=$redirectedTempFolder/CCLE_tpm_Clinical.tsv
+drugData=$redirectedTempFolder/CCLE_NP24.2009_Drug_data_2015.02.24.csv
+profilingData=$redirectedTempFolder/CCLE_NP24.2009_profiling_2012.02.20.csv
+meta1=$redirectedTempFolder/CCLE_copynumber_byGene_2013-12-03.txt
+meta2=$redirectedTempFolder/ccle2maf_20170805f.txt
 dataOutFile=data.tsv
 metadataOutFile=metadata.tsv
 dataOutFilegz=data.tsv.gz
@@ -11,7 +15,13 @@ metadataOutFilegz=metadata.tsv.gz
 rm -f $metadataOutFilegz
 rm -f $dataOutFilegz
 
-python parse.py $expressionData $clinicalAnnotations $dataOutFile $metadataOutFile
+python parse.py $expressionData $clinicalAnnotations $dataOutFile $metadataOutFile $drugData $profilingData
 
 gzip $metadataOutFile
 gzip $dataOutFile
+
+python3 parseMeta1.py $meta1 $dataOutFilegz
+python3 parseMeta2.py $meta2 $metadataOutFilegz
+
+rm -f $tempMeta1
+rm -f $tempMeta2
