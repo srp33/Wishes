492c47e87303a8467dd3ae111a5f4dff34582916
Benjamin Cookson <btc36@m8int02.fsl.byu.edu>
2018-03-30 15:02:30 -0600

    updated Code

--- CCLE_mRNA_isoform_kallisto_Tatlow/parse.py
+++ CCLE_mRNA_isoform_kallisto_Tatlow/parse.py
@@ -97,3 +97,4 @@ with open(dataOutFile, 'w') as ofData:
             except KeyError: 
                  continue #This will catch the following mismatches: RS4_11, EKVX, SF539, SNB75, SF268, MOLT-3, HOP-92, HOP-62, UO-31, WM983B, HOP-62, EKVX
 
+
