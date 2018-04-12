84c2cee1ee0c7cc5cf7de43b713dc2d8fa848e72
Benjamin Cookson <btc36@m8int01.fsl.byu.edu>
2018-04-12 17:12:38 -0600

    added CNV__

--- CCLE_mRNA_isoform_kallisto_Tatlow/parse.sh
+++ CCLE_mRNA_isoform_kallisto_Tatlow/parse.sh
@@ -5,7 +5,7 @@ expressionData=$redirectedTempFolder/CCLE_tpm_Expression.tsv
 clinicalAnnotations=$redirectedTempFolder/CCLE_tpm_Clinical.tsv
 drugData=$redirectedTempFolder/CCLE_NP24.2009_Drug_data_2015.02.24.csv  
 profilingData=$redirectedTempFolder/CCLE_NP24.2009_profiling_2012.02.20.csv
-meta1=$redirectedTempFolder/CCLE_copynumber_byGene_2013-12-03.txt
+data1=$redirectedTempFolder/CCLE_copynumber_byGene_2013-12-03.txt
 meta2=$redirectedTempFolder/ccle2maf_20170805f.txt
 dataOutFile=data.tsv
 metadataOutFile=metadata.tsv
@@ -20,6 +20,6 @@ python3 parse.py $expressionData $clinicalAnnotations $dataOutFile $metadataOutF
 gzip $metadataOutFile
 gzip $dataOutFile
 
-python3 parseMeta1.py $meta1 $dataOutFilegz
+python3 parseData1.py $data1 $dataOutFilegz
 python3 parseMeta2.py $meta2 $metadataOutFilegz
 python3 keep_common_samples.py $metadataOutFilegz $dataOutFilegz
