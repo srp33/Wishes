a088d6992c3fcdcdf29937e154a882a61d172aa2
Daniel Larson <dzlarson332@gmail.com>
2018-06-20 09:13:39 -0600

    Modification of install and parse scripts to include convertTallFormatToWide.py

--- TCGA_BreastCancer_CNV/parse.sh
+++ TCGA_BreastCancer_CNV/parse.sh
@@ -16,6 +16,7 @@ patientCancerType=$redirectedTemp/"GSE62944_06_01_15_TCGA_24_CancerType_Samples.
 #OutFile
 dataOutFilegz=data.tsv
 metadataOutFilegz=metadata.tsv
+convertedMetaOut=Clincal.tsv.gz
 #Values is a file previously created by matches.py that contains the common samples of data and metadata
 commonValues=values
 
@@ -32,4 +33,7 @@ gzip $metadataOutFilegz
 
 #To insure only common samples
 python3 keep_common_samples.py "metadata.tsv.gz" "data.tsv.gz"
+
+#Conversion of metadata to wide format
+python3 convertTallFormatToWide.py "metadata.tsv.gz" $convertedMetaOut
 #source deactivate WishBuilderDependencies
