0ef0787ef10e605e689ab0f4a6f2409f6797c0d6
Daniel Larson <dzlarson332@gmail.com>
2018-07-23 10:50:44 -0600

    Modification of install to remove conda install lines

--- CMAP_2_Entrez/install.sh
+++ CMAP_2_Entrez/install.sh
@@ -3,8 +3,8 @@
 cp ../Helper/convertTallFormatToWide.py .
 #install all the R packages in the environment
 #source activate WishBuilderDependencies 
-conda install -y -c bioconda r-sleuth 
-conda install -y -c r r-xml=3.98_1.5
-conda install -y gcc
+#conda install -y -c bioconda r-sleuth 
+#conda install -y -c r r-xml=3.98_1.5
+#conda install -y gcc
 Rscript installRPackages.R
 #source deactivate WishBuilderDependencies
