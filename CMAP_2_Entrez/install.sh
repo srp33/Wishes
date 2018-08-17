b56768b476ca7d16cf52973b161675861b598afd
Daniel Larson <dzlarson332@gmail.com>
2018-08-17 11:31:05 -0600

    Comment out of installRPackages.R

--- CMAP_2_Entrez/install.sh
+++ CMAP_2_Entrez/install.sh
@@ -6,5 +6,5 @@ cp ../Helper/convertTallFormatToWide.py .
 #conda install -y -c bioconda r-sleuth 
 #conda install -y -c r r-xml=3.98_1.5
 #conda install -y gcc
-Rscript installRPackages.R
+#Rscript installRPackages.R
 #source deactivate WishBuilderDependencies
