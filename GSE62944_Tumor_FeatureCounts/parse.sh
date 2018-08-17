29293e10e6aed70be8367f009c38b0cd7d36de40
srp33 <stephen_piccolo@byu.edu>
2018-08-17 15:39:14 -0600

    Cleaned up some HTML scraping commands and changed name of test file.

--- GSE62944_Tumor_FeatureCounts/parse.sh
+++ GSE62944_Tumor_FeatureCounts/parse.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
+set -o errexit
+
 redirectedTempFolder=tmp
 metaData=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz
 patientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
 tumorFeatureCounts=$redirectedTempFolder/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_FeatureCounts.txt.gz
-tcgaHtml=$redirectedTempFolder/"tcga_abbreviations.html"
-nameToAbbreviation="nameToAbbreviation.txt"
+nameToAbbreviation=nameToAbbreviation.txt
 dataOut=data.tsv.gz
 metadataOut=metadata.tsv.gz
 metadataTranslated=Clinical.tsv.gz
 
-#source activate WishBuilderDependencies
-
 python3 parse.py $metaData $patientCancerType $tumorFeatureCounts $dataOut $metadataOut $nameToAbbreviation
 python3 convertTallFormatToWide.py $metadataOut $metadataTranslated
+rm -f $metadataOut
