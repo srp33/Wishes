58ccfbba6f6c0a6c3ea03c80fe05aed0ff3e17e1
srp33 <stephen_piccolo@byu.edu>
2018-08-17 17:45:13 -0600

    Renamed output files and fine tuned cleanup.

--- GSE62944_Tumor_TPM/parse.sh
+++ GSE62944_Tumor_TPM/parse.sh
@@ -5,10 +5,9 @@ metaData=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_
 patientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
 tumorTPM=$redirectedTempFolder/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz
 nameToAbbreviation="nameToAbbreviation.txt"
-dataOut=data.tsv.gz
+dataOut=Gene_Expression.tsv.gz
 metadataOut=metadata.tsv.gz
 metaDataConverted=Clinical.tsv.gz
 
 python3 parse.py $metaData $patientCancerType $tumorTPM $dataOut $metadataOut $nameToAbbreviation
 python3 convertTallFormatToWide.py $metadataOut $metaDataConverted
-rm -f $metadataOut
