23ed5f67cf6d36922315a81335563eed48ffbde4
srp33 <stephen_piccolo@byu.edu>
2018-08-20 16:57:41 -0600

    Modified for test

--- METABRIC_cBio/parse.sh
+++ METABRIC_cBio/parse.sh
@@ -5,6 +5,7 @@ set -euo pipefail
 python3 ParseClinical.py "tmp/data_clinical_sample.txt" "tmp/data_clinical_patient.txt" "tmp/data_CNA.txt" "tmp/data_mutations_extended.txt" "tmp/Clinical.tsv.gz"
 ###################
 mv tmp/Clinical.tsv.gz .
+echo "This is a test........................................................................................................................"
 ###################
 
 #python3 ParseExpression.py "tmp/data_expression.txt" "Gene_Expression.tsv.gz"
