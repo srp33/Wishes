f94846c027068bc5986093074138c825fc0548a6
srp33 <stephen_piccolo@byu.edu>
2018-08-22 21:31:33 -0600

    Minor tweaks

--- GSE62944_Tumor_TPM/parse.sh
+++ GSE62944_Tumor_TPM/parse.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
 
-redirectedTempFolder=tmp
-metaData=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz
-patientCancerType=$redirectedTempFolder/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
-tumorTPM=$redirectedTempFolder/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz
+metaData=tmp/GSE62944_06_01_15_TCGA_24_548_Clinical_Variables_9264_Samples.txt.gz
+patientCancerType=tmp/GSE62944_06_01_15_TCGA_24_CancerType_Samples.txt.gz
+tumorTPM=tmp/GSM1536837_06_01_15_TCGA_24.tumor_Rsubread_TPM.txt.gz
 nameToAbbreviation="nameToAbbreviation.txt"
 dataOut=Gene_Expression.tsv.gz
 metadataOut=metadata.tsv.gz
