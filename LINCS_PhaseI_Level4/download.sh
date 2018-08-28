9bff6d5521f4b208c9ca89d7fc15c13b742eed1e
srp33 <stephen_piccolo@byu.edu>
2018-08-27 22:54:10 -0600

    Minor tweaks

--- LINCS_PhaseI_Level4/download.sh
+++ LINCS_PhaseI_Level4/download.sh
@@ -17,7 +17,7 @@ downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=fil
 downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742%5FBroad%5FLINCS%5Fcell%5Finfo%2Etxt%2Egz" "GSE92742_Broad_LINCS_cell_info.txt.gz"
 downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742%5FBroad%5FLINCS%5Fpert%5Finfo%2Etxt%2Egz" "GSE92742_Broad_LINCS_pert_info.txt.gz"
 downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742%5FBroad%5FLINCS%5Fpert%5Fmetrics%2Etxt%2Egz" "GSE92742_Broad_LINCS_pert_metrics.txt.gz"
-#downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742%5FBroad%5FLINCS%5FLevel4%5FZSPCINF%5Fmlr12k%5Fn1319138x12328%2Egctx%2Egz" "LINCS_PhaseI_Level4.gctx.gz"
+downloadFile "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE92742&format=file&file=GSE92742%5FBroad%5FLINCS%5FLevel4%5FZSPCINF%5Fmlr12k%5Fn1319138x12328%2Egctx%2Egz" "LINCS_PhaseI_Level4.gctx.gz"
 
-#cd tmp
-#gunzip "LINCS_PhaseI_Level4.gctx.gz" 
+cd tmp
+gunzip "LINCS_PhaseI_Level4.gctx.gz" 
--- LINCS_PhaseI_Level4/parse.sh
+++ LINCS_PhaseI_Level4/parse.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-instInfoFileName=$redirectedTempFolder/GSE92742_Broad_LINCS_inst_info.txt.gz
-gctxFileName=$redirectedTempFolder/LINCS_PhaseI_Level4.gctx
-geneFile=$redirectedTempFolder/GSE92742_Broad_LINCS_gene_info.txt.gz
-cellInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_cell_info.txt.gz
-pertInfo=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_info.txt.gz	
-pertMetrics=$redirectedTempFolder/GSE92742_Broad_LINCS_pert_metrics.txt.gz
+set -o errexit
+
+instInfoFileName=tmp/GSE92742_Broad_LINCS_inst_info.txt.gz
+gctxFileName=tmp/LINCS_PhaseI_Level4.gctx
+geneFile=tmp/GSE92742_Broad_LINCS_gene_info.txt.gz
+cellInfo=tmp/GSE92742_Broad_LINCS_cell_info.txt.gz
+pertInfo=tmp/GSE92742_Broad_LINCS_pert_info.txt.gz	
+pertMetrics=tmp/GSE92742_Broad_LINCS_pert_metrics.txt.gz
 
 python ParseMetadata.py $instInfoFileName $cellInfo $pertInfo $pertMetrics tmp/Metadata.tsv.gz
 python convertTallFormatToWide.py tmp/Metadata.tsv.gz Metadata.tsv.gz
